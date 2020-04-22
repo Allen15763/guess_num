49e1743104bdf9c483af6fa766db48e4b3a17b77
Allen Li <love22798800@gmail.com>
2020-04-23 00:09:03 +0800

    add description-times

--- guess_num.py
+++ guess_num.py
@@ -1,15 +1,18 @@
 #猜數字
 import random
 r = random.randint(1, 100)
-
+count = 0
 while True:
+	count += 1 # count = count + 1
 	text = input('Please tpye a number: ')
 	text = int(text)
 	if text == r:
 		print('You got it!')
+		print('This is', count, 'times')
 		break
 	elif r < text:
 		print('smallar')
 	elif r > text:
 		print('bigger')
+	print('This is', count, 'times')
 
